d6c5387f2e6a77c8c4f26537120b7c822ee21fc4
takred <sazonov.kos@yandex.ru>
2021-05-17 15:03:26 +0300

    Revert "Для каждого if сделан отдельный класс."
    This reverts commit e269c7a3

--- src/main/java/takred/weightcontrol/Bot.java
+++ src/main/java/takred/weightcontrol/Bot.java
@@ -1,6 +1,8 @@
 package takred.weightcontrol;
 
 import lombok.SneakyThrows;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.CategoryDataset;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
@@ -11,13 +13,16 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-import takred.weightcontrol.bot_commands.*;
+import takred.weightcontrol.dto.UserNameAndWeightDto;
 import takred.weightcontrol.dto.WeightDto;
 import takred.weightcontrol.service.WeightService;
 
 import javax.annotation.PostConstruct;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -26,22 +31,10 @@ import java.util.List;
 
 @Service
 public class Bot extends TelegramLongPollingBot {
-    public final WeightService weightService;
-    private final AddWeight addWeight;
-    private final GetChartByButton getChartByButton;
-    private final GetWeightList getWeightList;
-    private final GetChartByCommand getChartByCommand;
-    private final GetButtons getButtons;
-    private final RedactWeight redactWeight;
+    private final WeightService weightService;
 
-    public Bot(WeightService weightService, AddWeight addWeight, GetChartByButton getChartByButton, GetWeightList getWeightList, GetChartByCommand getChartByCommand, GetButtons getButtons, RedactWeight redactWeight) {
+    public Bot(WeightService weightService) {
         this.weightService = weightService;
-        this.addWeight = addWeight;
-        this.getChartByButton = getChartByButton;
-        this.getWeightList = getWeightList;
-        this.getChartByCommand = getChartByCommand;
-        this.getButtons = getButtons;
-        this.redactWeight = redactWeight;
     }
 
     @PostConstruct
@@ -64,7 +57,7 @@ public class Bot extends TelegramLongPollingBot {
         try {
             sendApiMethod(sendMessage);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
@@ -76,7 +69,7 @@ public class Bot extends TelegramLongPollingBot {
         try {
             execute(sendPhoto);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
@@ -88,7 +81,7 @@ public class Bot extends TelegramLongPollingBot {
         try {
             execute(sendPhoto);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
@@ -100,7 +93,7 @@ public class Bot extends TelegramLongPollingBot {
         try {
             execute(sendPhoto);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
@@ -118,7 +111,7 @@ public class Bot extends TelegramLongPollingBot {
         try {
             execute(new SendMessage().setChatId(message.getChatId()).setText("Список команд").setReplyMarkup(inlineKeyboardMarkup));
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
     }
 
@@ -128,49 +121,66 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         System.out.println("111");
         Message message = update.getMessage();
-        if (message == null) {
-            if (update.hasCallbackQuery()) {
-                if (update.getCallbackQuery().getData().equals("/gwc")) {
-                    System.out.println("777");
-                    getChartByButton.getChart(this, update);
+        if (message != null) {
+            System.out.println("222");
+            if (message.getText().equals("/gw")) {
+                System.out.println("333");
+                sendMessage(message, weightService.getMyWeight(message.getFrom().getId().toString()).toString());
+            } else if (message.getText().equals("/gwc")) {
+                System.out.println("444");
+                ChartCreator chartCreator = new ChartCreator();
+                CategoryDataset categoryDataset = chartCreator.createDataset(weightService.getMyWeight(message.getFrom().getId().toString()));
+                JFreeChart chart = chartCreator.createChart(categoryDataset);
+                BufferedImage bufferedImage = chart.createBufferedImage(1000, 1000);
+                File outputfile = new File("chart.png");
+                ImageIO.write(bufferedImage, "png", outputfile);
+                sendPhoto(message, outputfile);
+            } else if (message.getText().equals("/gwp")) {
+                System.out.println("555");
+                sendPhoto(message);
+            } else if (message.getText().equals("b")) {
+                System.out.println("666");
+                sendInlineKeyboardButton(message);
+            } else {
+                System.out.println("888");
+                LocalDateTime dateTime = LocalDateTime.now();
+                LocalTime midday = LocalTime.of(12, 00);
+
+                List<WeightDto> weights = weightService.getMyWeight(message.getFrom().getId().toString());
+                if (!weights.isEmpty()) {
+                    System.out.println("881");
+                    WeightDto obj = weights.get(weights.size() - 1);
+                    System.out.println(obj);
+                    boolean now = Integer.valueOf(dateTime.toLocalTime().getHour()) >= Integer.valueOf(midday.getHour());
+                    boolean inRecording = Integer.valueOf(obj.getDate().toLocalTime().getHour()) >= Integer.valueOf(midday.getHour());
+                    if (dateTime.toLocalDate().equals(obj.getDate().toLocalDate())
+                            && now == inRecording) {
+                        System.out.println("882");
+                        System.out.println(obj.getId());
+                        weightService.setWeight(obj, Double.parseDouble(message.getText()));
+                        sendMessage(message, "Показатель веса отредактирован.");
+                    } else {
+                        System.out.println("883");
+                        weightService.addWeight(new UserNameAndWeightDto(message.getFrom().getId().toString(), Double.parseDouble(message.getText())));
+                        sendMessage(message, "Показатель веса добавлен в список.");
+                    }
+                } else {
+                    System.out.println("883");
+                    weightService.addWeight(new UserNameAndWeightDto(message.getFrom().getId().toString(), Double.parseDouble(message.getText())));
+                    sendMessage(message, "Показатель веса добавлен в список.");
                 }
             }
-            return;
-        }
-        System.out.println("222");
-        if (message.getText().equals("/gw")) {
-            System.out.println("333");
-            getWeightList.getWeightList(this, message);
-        } else if (message.getText().equals("/gwc")) {
-            System.out.println("444");
-            getChartByCommand.getChartByCommand(this, message);
-        } else if (message.getText().equals("b")) {
-            System.out.println("666");
-            getButtons.getButtons(this, message);
-        } else {
             System.out.println("888");
-            LocalDateTime dateTime = LocalDateTime.now();
-            LocalTime midday = LocalTime.of(12, 00);
-
-            List<WeightDto> weights = weightService.getMyWeight(message.getFrom().getId().toString());
-            if (weights.isEmpty()) {
-                System.out.println("883");
-                addWeight.addWeight(this, message);
-                return;
-            }
-            System.out.println("881");
-            WeightDto obj = weights.get(weights.size() - 1);
-            System.out.println(obj);
-            boolean now = Integer.valueOf(dateTime.toLocalTime().getHour()) >= Integer.valueOf(midday.getHour());
-            boolean inRecording = Integer.valueOf(obj.getDate().toLocalTime().getHour()) >= Integer.valueOf(midday.getHour());
-            if (dateTime.toLocalDate().equals(obj.getDate().toLocalDate())
-                    && now == inRecording) {
-                System.out.println("882");
-                System.out.println(obj.getId());
-                redactWeight.redactWeight(this, message, obj);
-            } else {
-                System.out.println("883");
-                addWeight.addWeight(this, message);
+        } else if (update.hasCallbackQuery()) {
+            if (update.getCallbackQuery().getData().equals("/gwc")) {
+                System.out.println("777");
+                ChartCreator chartCreator = new ChartCreator();
+                CategoryDataset categoryDataset = chartCreator.createDataset(weightService.getMyWeight(update.getCallbackQuery().getFrom().getId().toString()));
+                JFreeChart chart = chartCreator.createChart(categoryDataset);
+                BufferedImage bufferedImage = chart.createBufferedImage(1000, 1000);
+                File outputfile = new File("chart.png");
+                ImageIO.write(bufferedImage, "png", outputfile);
+                sendPhoto(update.getCallbackQuery(), outputfile);
             }
         }
     }

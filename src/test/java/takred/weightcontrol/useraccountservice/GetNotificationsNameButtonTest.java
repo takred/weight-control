cbe2410b7ae5ce17765275606094fb565e7a44bc
takred <sazonov.kos@yandex.ru>
2021-06-25 14:55:39 +0300

    Добавлен тест для GetNotificationsNameButton.

---
+++ src/test/java/takred/weightcontrol/useraccountservice/GetNotificationsNameButtonTest.java
@@ -0,0 +1,42 @@
+package takred.weightcontrol.useraccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import takred.weightcontrol.entity.UserAccount;
+import takred.weightcontrol.repository.UserAccountRepository;
+import takred.weightcontrol.service.UserAccountService;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+public class GetNotificationsNameButtonTest {
+    @Mock
+    UserAccountRepository userAccountRepository;
+
+    @InjectMocks
+    UserAccountService userAccountService;
+
+    @Test
+    void notificationsOn() {
+        UserAccount userAccount = new UserAccount(1, 123L);
+        userAccount.setSendNotifications(true);
+        Optional<UserAccount> optionalUserAccount = Optional.of(userAccount);
+        doReturn(optionalUserAccount).when(userAccountRepository).findById(1);
+        assertEquals("Выключить уведомления", userAccountService.getNotificationsNameButton(1));
+    }
+
+    @Test
+    void notificationsOff() {
+        UserAccount userAccount = new UserAccount(1, 123L);
+        userAccount.setSendNotifications(false);
+        Optional<UserAccount> optionalUserAccount = Optional.of(userAccount);
+        doReturn(optionalUserAccount).when(userAccountRepository).findById(1);
+        assertEquals("Включить уведомления", userAccountService.getNotificationsNameButton(1));
+    }
+}

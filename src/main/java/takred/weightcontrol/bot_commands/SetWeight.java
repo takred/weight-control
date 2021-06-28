ede768bf710629ebff4aec1b395947cbd18ab398
takred <sazonov.kos@yandex.ru>
2021-06-28 23:18:15 +0300

    Введено ограничение на запись веса по сравнению с значением веса в предыдущей записи.

--- src/main/java/takred/weightcontrol/bot_commands/SetWeight.java
+++ src/main/java/takred/weightcontrol/bot_commands/SetWeight.java
@@ -1,5 +1,6 @@
 package takred.weightcontrol.bot_commands;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import takred.weightcontrol.Bot;
@@ -16,11 +17,17 @@ public class SetWeight implements MessageHandler {
     private final WeightService weightService;
     private final AddWeight addWeight;
     private final RedactWeight redactWeight;
+    private final Double recordBoundary;
 
-    public SetWeight(WeightService weightService, AddWeight addWeight, RedactWeight redactWeight) {
+    public SetWeight(WeightService weightService,
+                     AddWeight addWeight,
+                     RedactWeight redactWeight,
+                     @Value("${record-boundary}") String recordBoundary) {
         this.weightService = weightService;
         this.addWeight = addWeight;
         this.redactWeight = redactWeight;
+        this.recordBoundary = Double.parseDouble(recordBoundary);
+
     }
 
     public boolean process(Bot bot, Update update) {
@@ -32,21 +39,32 @@ public class SetWeight implements MessageHandler {
                     System.out.println("Couldn't turn string into number");
                     return false;
                 }
+                List<WeightDto> weights = weightService.getMyWeight(update.getMessage().getFrom().getId().toString());
+                WeightDto obj = weights.get(weights.size() - 1);
+                Double newWeight = Double.parseDouble(update.getMessage().getText().replace(" ", "."));
+                boolean withinTheBorder = Math.abs(obj.getWeight() - newWeight) > 0
+                        && Math.abs(obj.getWeight() - newWeight) <= recordBoundary;
+                if (!withinTheBorder) {
+                    bot.sendMessage(update.getMessage(), "Введённый вес больше предыдущего на 5кг. Такого не может быть!");
+                    return true;
+                }
+
                 LocalDateTime dateTime = LocalDateTime.now();
                 LocalTime midday = LocalTime.of(12, 00);
 
-                List<WeightDto> weights = weightService.getMyWeight(update.getMessage().getFrom().getId().toString());
                 if (addWeight.addWeight(bot, update.getMessage(), weights)) {
                     return true;
                 }
-                WeightDto obj = weights.get(weights.size() - 1);
+
                 boolean now = dateTime.toLocalTime().getHour() >= midday.getHour();
                 boolean inRecording = obj.getDate().toLocalTime().getHour() >= midday.getHour();
                 boolean condition = dateTime.toLocalDate().equals(obj.getDate().toLocalDate())
                         && now == inRecording;
+
                 if (addWeight.addWeight(bot, update.getMessage(), condition)) {
                     return true;
                 }
+
                 redactWeight.redactWeight(bot, update.getMessage(), obj);
                 return true;
             }
